3825b7e1f85d1ca8accca7feb12951f135dce0d1
hjribeiro <47180787+hjribeiro@users.noreply.github.com>
2019-04-05 11:34:54 +0100

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,6 +1,23 @@
 def func(a, b, operation):
-    print (operation(a, b))
+    return operation(a, b)
 
 
-func(2, 10, lambda a, b : a + b)   
-func(2, 10, lambda a, b : a * b)
+print(func(2, 10, lambda a, b : a + b))
+print(func(2, 10, lambda a, b : a * b))
+
+func("Hello ", "Dan", print)
+
+
+def upload_file(file, bucket_type):
+    print("will upload file")
+    bucket_type(file)
+    
+
+def upload_to_s3(file):
+    print("uploading to S3")
+
+def upload_to_gcp(file):
+    print("uploading to GCP")
+
+
+upload_file("test_file", upload_to_s3)
